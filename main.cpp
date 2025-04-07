1ac676378a36525eaaaa4a61f1a6a4bb46a80c67
Jared <jared@web.net>
2025-04-07 13:38:39 -0400

    moved init, close and loadMedia to initialize.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,12 @@
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>
 #include <stdio.h>
 #include <iostream>
 #include <string>
 #include <cmath>  // For sqrt and atan2
 #include <vector>
-#include "sprite_data.h"
 #include "player.h"
 #include "enemy.h"
 #include <typeinfo>
+#include "initialize.cpp"
 
 // const int SCREEN_WIDTH = 1920;
 // const int SCREEN_HEIGHT = 1080;
@@ -22,113 +20,6 @@ int posY = windowHeight / 2;
 const int ENEMY_SIZE = gCupcakeTexture.getWidth();  
 int ENEMY_WIDTH = gCupcakeTexture.getWidth();
 int ENEMY_HEIGHT = gCupcakeTexture.getHeight();
-
-
-//Starts up SDL and creates window
-bool init();
-
-//Loads media
-bool loadMedia();
-
-//Frees media and shuts down SDL
-void close();
-
-//The window we'll be rendering to
-SDL_Window* gWindow = NULL;
-
-bool init()
-{
-	//Initialization flag
-	bool success = true;
-
-	//Initialize SDL
-	if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
-	{
-		printf( "SDL could not initialize! SDL Error: %s\n", SDL_GetError() );
-		success = false;
-	}
-	else
-	{
-		//Set texture filtering to linear
-		if( !SDL_SetHint( SDL_HINT_RENDER_SCALE_QUALITY, "1" ) )
-		{
-			printf( "Warning: Linear texture filtering not enabled!" );
-		}
-
-		//Create window
-		gWindow = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1920, 1080, SDL_WINDOW_MAXIMIZED | SDL_WINDOW_SHOWN );
-
-		if( gWindow == NULL )
-		{
-			printf( "Window could not be created! SDL Error: %s\n", SDL_GetError() );
-			success = false;
-		}
-		else
-		{
-			//Create vsynced renderer for window
-			gRenderer = SDL_CreateRenderer( gWindow, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC );
-			if( gRenderer == NULL )
-			{
-				printf( "Renderer could not be created! SDL Error: %s\n", SDL_GetError() );
-				success = false;
-			}
-			else
-			{
-				//Initialize renderer color
-				SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
-
-				//Initialize PNG loading
-				int imgFlags = IMG_INIT_PNG;
-				if( !( IMG_Init( imgFlags ) & imgFlags ) )
-				{
-					printf( "SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError() );
-					success = false;
-				}
-			}
-		}
-	}
-	return success;
-}
-
-bool loadMedia()
-{
-	//Loading success flag
-	bool success = true;
-
-	//Load sprite sheet texture
-	if( !gSpriteSheetTexture.loadFromFile( "HC_Humans1A_4x.png" ) )
-	{
-		printf( "Failed to load walking animation texture!\n" );
-		success = false;
-	}
-	if( !gCupcakeTexture.loadFromFile( "cupcake.png" ) )
-    {
-        printf( "Failed to load enemy texture!\n" );
-        success = false;
-    }
-	else
-	{
-		//Set sprite clips from sprite_data.h
-        animateSriteDown();
-        animateSriteLeft();
-        animateSriteRight();
-        animateSriteUp();
-	}
-	return success;
-}
-
-void close()
-{
-	//Destroy window	
-	SDL_DestroyRenderer( gRenderer );
-	SDL_DestroyWindow( gWindow );
-	gWindow = NULL;
-	gRenderer = NULL;
-
-	//Quit SDL subsystems
-	IMG_Quit();
-	SDL_Quit();
-}
 
 int main( int argc, char* args[] )
 {
